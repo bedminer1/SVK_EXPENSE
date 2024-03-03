80cd456b52f7d6142e837ae7420d9292e48f2975
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-03-03 14:49:40 +0800

    custom theme implemented

--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -1,11 +1,14 @@
+// @ts-ignore
 import { join } from 'path'
 import type { Config } from 'tailwindcss'
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import { skeleton } from '@skeletonlabs/tw-plugin'
+import { myCustomTheme } from './my-custom-theme';
 
 export default {
 	darkMode: 'class',
+	// @ts-ignore
 	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
 	theme: {
 		extend: {},
@@ -15,15 +18,8 @@ export default {
 		typography,
 		skeleton({
 			themes: {
-				preset: [
-					{
-						name: 'skeleton',
-						enhancements: true,
-					},
-					{
-						name: 'sahara',
-						enhancements: true,
-					},
+				custom: [
+					myCustomTheme
 				],
 			},
 		}),
