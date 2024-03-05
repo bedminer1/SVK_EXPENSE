72462d7c5469c9d44c2dcf7c751cde2ac04f0a37
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-03-06 00:56:59 +0800

    refactored display card

---
+++ my-app/src/lib/components/ExpenseDisplayCard.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { enhance } from "$app/forms";
+
+    export let record: Expense
+</script>
+
+<div class="card p-2 w-1/3">
+    <header class="card-header">
+        {record.category}
+        <div class="float-right">
+            <form action="?/delete" method="post" use:enhance>
+                <input type="hidden" class="id" name="id" value={record.id}>
+                <button type="submit" class="btn btn-sm variant-filled-error">Delete</button>
+            </form>
+        </div>
+    </header>
+    <section class="p-4">
+        <p>{record.title}</p>
+        <br>
+        <p>${record.amount.toFixed(2)}</p>
+    </section>
+</div>
--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import ExpenseInput from '$lib/components/ExpenseInput.svelte'
+	import ExpenseDisplayCard from '$lib/components/ExpenseDisplayCard.svelte'
 	export let data
 	const { records } = data
 </script>
@@ -8,22 +9,7 @@
 <div class="flex flex-col container justify-center items-center gap-4">
 	<ExpenseInput />	
 	{#each records as record} 
-		<div class="card p-2 w-1/3">
-			<header class="card-header">
-				{record.category}
-				<div class="float-right">
-					<form action="?/delete" method="post">
-						<input type="hidden" class="id" name="id" value={record.id}>
-						<button type="submit" class="btn btn-sm variant-filled-error">Delete</button>
-					</form>
-				</div>
-			</header>
-			<section class="p-4">
-				<p>{record.title}</p>
-				<br>
-				<p>${record.amount.toFixed(2)}</p>
-			</section>
-		</div>
+		<ExpenseDisplayCard {record}/>
 	{/each}
 </div>
 
