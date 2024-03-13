5a69fb36bcee7d6348dec041d0e4e0731db7210c
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-03-14 00:16:17 +0800

    changed it up to be log in only

--- my-app/src/routes/login/+page.server.ts
+++ my-app/src/routes/login/+page.server.ts
@@ -9,30 +9,6 @@ export const load = async ({ locals }) => {
 };
 
 export const actions = {
-	register: async ({ locals, request }) => {
-		const data = await request.formData();
-		const email = data.get('email');
-		const password = data.get('password');
-
-		// check email and password not blank
-		if (!email || !password) {
-			return fail(400, { emailRequired: email === null, passwordRequired: password === null });
-		}
-
-		data.set('passwordConfirm', password?.toString());
-
-		try {
-			await locals.pb.collection('users').create(data);
-			await locals.pb.collection('users').authWithPassword(email.toString(), password.toString());
-			await locals.pb.collection('users').requestVerification(email.toString());
-		} catch (error) {
-			const errorObj = error as ClientResponseError;
-			return fail(500, { fail: true, message: errorObj.data.message });
-		}
-
-		throw redirect(303, '/');
-	},
-
 	login: async ({ locals, request }) => {
 		const data = await request.formData();
 		const email = data.get('email');
@@ -49,6 +25,6 @@ export const actions = {
 			return fail(500, { fail: true, message: errorObj.data.message });
 		}
 
-		throw redirect(303, '/');
+		throw redirect(303, '/dashboard');
 	}
 };
