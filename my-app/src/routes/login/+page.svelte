e1a11ef8b81170f929d31732aae6aca457009357
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-04-22 22:16:29 +0800

    submitting and delayed stores from superforms used for login page delay management

--- my-app/src/routes/login/+page.svelte
+++ my-app/src/routes/login/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-    import { superForm, type FormOptions } from 'sveltekit-superforms'
+    import { superForm } from 'sveltekit-superforms'
+    import { ProgressRadial } from '@skeletonlabs/skeleton'
 
     export let data
 
-    const { form, errors, message, reset, capture, restore, enhance } = superForm(data.form, {
+    const { form, errors, message, capture, restore, enhance, submitting, delayed } = superForm(data.form, {
         resetForm: false
     })
 
@@ -19,7 +20,7 @@
                 <span>{$errors.email}</span>
             {/if}
         </label>
-        <label class="label mt-4">
+        <label class="label my-4">
             <span>Password</span>
             <input class="input mb-3" name="password" bind:value={$form.password} type="password" placeholder="******"/>
             {#if $errors.password}
@@ -27,6 +28,9 @@
             {/if}
         </label>
         {#if $message}<h>{$message}</h>{/if}
-        <button class="btn variant-filled w-full mt-4" type="submit">Login</button>
+        <button class="btn variant-filled-primary w-full my-4" disabled={$submitting} type="submit">Login</button>
+        {#if $delayed} 
+            <p class="text-center">Loading page...</p>
+        {/if}
     </form>
 </div>
